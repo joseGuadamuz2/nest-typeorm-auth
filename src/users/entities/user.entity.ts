b259ecc4565ae3b5969cdc47ce6ea4882c631ccf
Jose_Guadamuz <jose.guadamuz2@ulatina.net>
2026-05-21 14:09:23 -0600

    fix: add non-null assertion to user ID property in User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -5,7 +5,7 @@ import { BaseModel } from '../../common/base.model';
 export class User extends BaseModel {
   // Declaramos el ID explícitamente en la entidad
   @PrimaryGeneratedColumn('uuid', { name: 'Id' })
-  id: string;
+  id!: string;
 
   @Column({ unique: true })
   email!: string;
