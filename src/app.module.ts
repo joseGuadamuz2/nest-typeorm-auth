da30c165d392bc6318d78bdf90e57f6775af03ad
Jose_Guadamuz <jose.guadamuz2@ulatina.net>
2026-05-21 17:47:18 -0600

    Recuperando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-
 
 @Module({
   imports: [
@@ -13,22 +12,26 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true, //off in production
+    // Forzamos a TypeORM a esperar las variables del .env
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // ¡Solo para desarrollo local!
+      }),
     }),
-    UsersModule, AuthModule],
-
+    
+    UsersModule, 
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
-  
 })
-
-
 export class AppModule {}
